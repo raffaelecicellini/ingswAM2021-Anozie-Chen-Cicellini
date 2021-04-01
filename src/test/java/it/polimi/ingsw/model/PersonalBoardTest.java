53d8daeecef113f25dd866b12bd34832539f9954
raffaelecicellini <raffaele.cicellini@mail.polimi.it>
2021-04-01 18:19:31 +0200

    Created PersonalBoardTest. Added constructor, getDeposits, setDeposits, swapDeposits, addSpecialDeposit, getSlot, getTopCard, addCard to the PersonalBoard class (NOT yet tested) with javadoc.

---
+++ src/test/java/it/polimi/ingsw/model/PersonalBoardTest.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+public class PersonalBoardTest {
+
+    @Test
+    public void depositsTest(){
+
+    }
+    @Test
+    public void developTest(){
+
+    }
+}

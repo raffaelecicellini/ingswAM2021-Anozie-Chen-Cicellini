cc2819ac9c2ab30633f4bd64b7d8821df33e3e5f
hansonobi9 <10581641@polimi.it>
2021-05-27 11:35:04 +0200

    added messages.

---
+++ src/main/java/it/polimi/ingsw/messages/MarketMessage.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.messages;
+
+import java.util.Map;
+
+public class MarketMessage extends Message{
+
+    public MarketMessage(Map<String, String> info) {
+        super(info);
+    }
+
+    @Override
+    public String getPos(int ind) {
+        return info.get("pos"+ind);
+    }
+
+    @Override
+    public String getRes(int ind) {
+        return info.get("res"+ind);
+    }
+
+    @Override
+    public boolean isRow() {
+        return info.containsKey("row");
+    }
+
+    @Override
+    public boolean isCol() {
+        return info.containsKey("col");
+    }
+
+    @Override
+    public int getMarblesIndex() {
+        return info.containsKey("row") ? Integer.parseInt(info.get("row")) : Integer.parseInt(info.get("col"));
+    }
+}
--- src/main/java/it/polimi/ingsw/messages/Message.java
+++ src/main/java/it/polimi/ingsw/messages/Message.java
@@ -3,13 +3,89 @@ package it.polimi.ingsw.messages;
 import java.util.Map;
 
 public abstract class Message {
-    protected Map<String,String> map;
-    private String player;
-    private String action;
+    protected Map<String,String> info;
+    private final String player;
+    private final String action;
 
-    public Message (Map<String, String> map){
-        player=map.remove("player");
-        action=map.remove("action");
-        this.map=map;
+    public Message (Map<String, String> info){
+        player=info.remove("player");
+        action=info.remove("action");
+        this.info=info;
+    }
+
+    public int getLeader(int ind) {
+        return -1;
+    }
+
+    public String getResource(int i) {
+        return null;
+    }
+
+    public String getPosition(int ind) {
+        return null;
+    }
+
+    public int getIndex() {
+        return -1;
+    }
+
+    public String getSource() {
+        return null;
+    }
+
+    public String getDest() {
+        return null;
+    }
+
+    public boolean isSelected(int ind) {
+        return false;
+    }
+
+    public String getRes(int prod, int res) {
+        return null;
+    }
+
+    public String getPos(int prod, int res) {
+        return null;
+    }
+
+    public String getOut(int prod) {
+        return null;
+    }
+
+    public String getPos(int ind) {
+        return null;
+    }
+
+    public String getRes(int ind) {
+        return null;
+    }
+
+    public boolean isRow() {
+        return false;
+    }
+
+    public boolean isCol() {
+        return false;
+    }
+
+    public int getMarblesIndex() {
+        return -1;
+    }
+
+    public int getCol() {
+        return -1;
+    }
+
+    public int getRow() {
+        return -1;
+    }
+
+    public int getSlot() {
+        return -1;
+    }
+
+    public Map<String,String> getCost() {
+        return null;
     }
 }
